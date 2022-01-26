81dae0d79a532649bf113ed73ce3868bcf1a028d
Estevão Libardi <estevaolibardi@hotmail.com>
2022-01-26 10:47:35 -0300

    feat:add vitrine dom

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,5 +1,9 @@
 import { ShopCart } from "../modules/ShopCart.js";
+import { Vitrine } from "../modules/Vitrine.js";
 
 const shopcart = new ShopCart()
 
 shopcart.updateCart()
+
+const vitrine = new Vitrine()
+vitrine.addCardsFromListToHtml()
