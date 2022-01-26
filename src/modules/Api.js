7adfe719c96b6e08a08c6ba22c64ba1d6f6b624e
leonardoliska <leo.liska@hotmail.com>
2022-01-26 10:34:58 -0300

    feat: get data from api

---
+++ src/modules/Api.js
@@ -0,0 +1,15 @@
+export { Api }
+
+class Api {
+    static endpoint = 'https://kenzie-food-api.herokuapp.com'
+    static token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjAsImlhdCI6MTY0MzExNzk3NiwiZXhwIjoxNjQzOTgxOTc2LCJzdWIiOiJbb2JqZWN0IFVuZGVmaW5lZF0ifQ.NP0ts9w-7_qN6cVH0I1PeaHY4fuZHLkhGREcKiDkbbY'
+
+    static async getAll() {
+        const data = fetch(`${this.endpoint}/my/product`, {
+            headers : {
+                Authorization: 'Bearer ' + this.token
+            }}).
+        then(res => res.json())
+        return data
+    }
+}
